670fdc87147452cb72633ac3200beb483232c202
rimanshupatel <rimanshupatel1@gmail.com>
2026-06-18 11:47:34 +0530

    Deploy Initial Project

---
+++ app/routes/home.tsx
@@ -0,0 +1,75 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/Navbar";
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import Button from "../components/ui/Button";
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Roomify 2.0</p>
+        </div>
+
+        <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+
+        <p className="subtitle">
+          Roomify is an AI-first design environment that helps you visualize, render, and ship architectural projects faster  than ever.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+
+            {/* <Upload onComplete={handleUploadComplete} /> */}
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared community projects, all in one place.</p>
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+    </div>
+  )
+}
